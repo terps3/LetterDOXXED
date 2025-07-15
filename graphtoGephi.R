4ba50fc3a7629d74d2337e95dd1a36d05c9074e3
Ferruccio Animali <122985838+terps3@users.noreply.github.com>
2025-07-15 22:34:54 +0200

    Update graphtoGephi.R

--- graphtoGephi.R
+++ graphtoGephi.R
@@ -7,6 +7,20 @@ g <- readRDS("movie_graph_full.rds")
 # Check it loaded correctly
 print(g)
 
+summary(g)
+
+# Check if 'community' exists as a vertex attribute
+if("community" %in% vertex_attr_names(g)) {  # CORRECTED LINE
+  # Communities are stored in the graph
+  communities <- V(g)$community
+  print("Communities found in graph attributes!")
+  print(paste("Number of communities:", length(unique(communities))))
+} else {
+  print("Communities not found in graph attributes!")
+  # You'll need to recalculate communities
+}
+summary(components(g))
+
 # Export to Gephi format
 # write_graph(g, "movie_graph_gephi.graphml", format = "graphml")
 
